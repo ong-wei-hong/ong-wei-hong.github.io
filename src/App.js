654f9c2a0a97300554ff0ce65f35219426fdef4f
ong-wei-hong <weihong.ong@outlook.com>
2022-06-06 09:05:22 +0800

    white at the bottom of mobile screen fix #1

--- src/App.js
+++ src/App.js
@@ -6,12 +6,19 @@ function App() {
     <div
       style={{
         margin: '0',
-        width: '100vw',
-        height: '100vh'
+        width: '100%',
+        height: '100%'
       }}
     >
-      <About />
-      <Background />
+      <div
+        style={{
+          height: '100vh',
+          width: '100vw'
+        }}
+      >
+        <About />
+        <Background />
+      </div>
     </div>
   );
 }
